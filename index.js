b74c5a614b087db16d797b0d392c74eec98279fe
thanhhn <thanhhn@antsomi.com>
2023-06-10 16:26:36 +0700

    change-domain-ui

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(cors());
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000', 'https://demo-website-ui-thanhhn23.vercel.app/*');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000', 'https://demo-website-ui-thanhhn23.vercel.app');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
